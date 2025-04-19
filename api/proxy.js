2d3907ed9089e5ab5d46a85b5ca2d0a94ae9fc22
7CCCCC21X <731495@qq.com>
2025-04-19 10:15:05 +0800

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const response = await fetch('https://acs-graphql.astar.network/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req.body),
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
